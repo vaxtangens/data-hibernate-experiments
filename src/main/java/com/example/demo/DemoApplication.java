d7c65f56b9fc4ee56b6ff9c9ed34939c9cbe73cc
p_popov <vaxtangens@gmail.com>
2020-01-02 21:23:51 +0300

    Miscellaneous changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+}
 
+class UuidUtil {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+    }
 }
